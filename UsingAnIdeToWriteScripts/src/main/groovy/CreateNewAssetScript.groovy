197e34697736fb26e3d3e19e23981d65dc0adbf2
Nick Smith <nickcodefresh@users.noreply.github.com>
2022-02-24 14:56:27 +0000

    Bumped com.ooyala.flex:flex-sdk-external and added code comment (#21)
    Co-authored-by: Nick Smith <nsmith@dalet.com>

--- UsingAnIdeToWriteScripts/src/main/groovy/CreateNewAssetScript.groovy
+++ UsingAnIdeToWriteScripts/src/main/groovy/CreateNewAssetScript.groovy
@@ -5,6 +5,8 @@ import com.ooyala.flex.sdk.FlexSdkClient
 
 class CreateNewAssetScript {
 
+    // These fields exist to help IDE auto-completion when writing scripts, and do not need to be included in your
+    // scripts, as the objects they represent are provided to the script automatically
     GroovyScriptContext context;
     FlexSdkClient flexSdkClient;
 
