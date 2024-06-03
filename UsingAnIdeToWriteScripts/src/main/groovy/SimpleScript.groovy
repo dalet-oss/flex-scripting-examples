e36906d76edb72dd87885d51bf34bba7ade16389
Nicholas Smith <nsmith@dalet.com>
2024-06-03 10:40:22 +0100

    Updated method sig

--- UsingAnIdeToWriteScripts/src/main/groovy/SimpleScript.groovy
+++ UsingAnIdeToWriteScripts/src/main/groovy/SimpleScript.groovy
@@ -1,12 +1,14 @@
+import com.ooyala.flex.jobexecutor.dto.FlexObjectReference
 import com.ooyala.flex.model.asset.create.NewAssetPlaceholder
 import com.ooyala.flex.plugins.GroovyScriptContext
+import com.ooyala.flex.plugins.actions.lock.ScriptingLockManager
 import com.ooyala.flex.sdk.FlexSdkClient
 
 
+def execute(List<FlexObjectReference> objectRefs, FlexSdkClient sdkClient, GroovyScriptContext context,
+            ScriptingLockManager lockManager) {
 
-def execute(FlexSdkClient flexSdkClient, GroovyScriptContext context) {
-
-    def assetService = flexSdkClient.getAssetService()
+    def assetService = sdkClient.getAssetService()
 
     def asset = NewAssetPlaceholder.builder()
             .type("media-asset")
