1e6d3adc7c09b4706c095e1e2921f77371f1ef0b
Nicholas Smith <nsmith@dalet.com>
2024-06-06 16:55:25 +0100

    Added another test script

---
+++ UsingAnIdeToWriteScripts/src/main/groovy/SimpleHttpMessageScript.groovy
@@ -0,0 +1,19 @@
+import com.ooyala.flex.jobexecutor.dto.FlexObjectReference
+import com.ooyala.flex.model.asset.create.NewAssetPlaceholder
+import com.ooyala.flex.plugins.GroovyScriptContext
+import com.ooyala.flex.plugins.actions.lock.ScriptingLockManager
+import com.ooyala.flex.sdk.FlexSdkClient
+
+def execute(FlexSdkClient flexSdkClient, GroovyScriptContext context, ScriptingLockManager lockManager,
+            List<FlexObjectReference> objectRefs, HttpRequestMessage) {
+
+    def assetService = flexSdkClient.getAssetService()
+
+    def asset = NewAssetPlaceholder.builder()
+            .type("media-asset")
+            .name("demo asset")
+            .build()
+
+    //assetService.createAsset(asset)
+
+}
