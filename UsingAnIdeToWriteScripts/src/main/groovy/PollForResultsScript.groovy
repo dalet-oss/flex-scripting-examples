b371a81500654ba734019d0a2848cbc963829692
Mackenzie Sepler <msepler@dalet.com>
2024-08-15 10:57:13 +0100

    remove import

--- UsingAnIdeToWriteScripts/src/main/groovy/PollForResultsScript.groovy
+++ UsingAnIdeToWriteScripts/src/main/groovy/PollForResultsScript.groovy
@@ -1,7 +1,6 @@
 import com.ooyala.flex.jobexecutor.dto.FlexObjectReference
 import com.ooyala.flex.plugins.GroovyScriptContext
 import com.ooyala.flex.sdk.FlexSdkClient
-import com.ooyala.flex.plugins.actions.lock.ScriptingLockManager
 
 
 def execute(FlexSdkClient flexSdkClient, GroovyScriptContext context, ScriptingLockManager lockManager,
