d43b530fd56358d8846325e37523b4741ae87345
Mackenzie Sepler <msepler@dalet.com>
2024-08-15 13:08:31 +0100

    add response

--- UsingAnIdeToWriteScripts/src/main/groovy/PollForResultsScript.groovy
+++ UsingAnIdeToWriteScripts/src/main/groovy/PollForResultsScript.groovy
@@ -1,10 +1,11 @@
 import com.ooyala.flex.jobexecutor.dto.FlexObjectReference
 import com.ooyala.flex.plugins.GroovyScriptContext
+import com.ooyala.flex.plugins.actions.impl.message.http.response.HttpResponse
 import com.ooyala.flex.sdk.FlexSdkClient
 import com.ooyala.flex.plugins.actions.service.scripting.lock.ScriptingLockManager
 
 
 def execute(FlexSdkClient flexSdkClient, GroovyScriptContext context, ScriptingLockManager lockManager,
-            List<FlexObjectReference> objectRefs) {
+            List<FlexObjectReference> objectRefs, HttpResponse httpResponse) {
     return "true"
 }
