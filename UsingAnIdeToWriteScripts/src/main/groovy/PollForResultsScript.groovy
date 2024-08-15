2cbfcbff3f5fbf2b1a29f325609dac157607df5d
Mackenzie Sepler <msepler@dalet.com>
2024-08-15 10:19:33 +0100

    put back imports

--- UsingAnIdeToWriteScripts/src/main/groovy/PollForResultsScript.groovy
+++ UsingAnIdeToWriteScripts/src/main/groovy/PollForResultsScript.groovy
@@ -1,4 +1,10 @@
+import com.ooyala.flex.jobexecutor.dto.FlexObjectReference
+import com.ooyala.flex.plugins.GroovyScriptContext
+import com.ooyala.flex.plugins.actions.lock.ScriptingLockManager
+import com.ooyala.flex.sdk.FlexSdkClient
+
+
 def execute(FlexSdkClient flexSdkClient, GroovyScriptContext context, ScriptingLockManager lockManager,
-            List<FlexObjectReference> objectRefs, HttpResponse obj) {
+            List<FlexObjectReference> objectRefs) {
     return "true"
 }
