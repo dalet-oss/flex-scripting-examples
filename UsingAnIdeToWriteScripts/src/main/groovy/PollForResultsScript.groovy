8cb9c94ce5f56d2fa7273240c634d98b9d087621
Mackenzie Sepler <msepler@dalet.com>
2024-08-15 11:10:23 +0100

    try no args

--- UsingAnIdeToWriteScripts/src/main/groovy/PollForResultsScript.groovy
+++ UsingAnIdeToWriteScripts/src/main/groovy/PollForResultsScript.groovy
@@ -1,9 +1,3 @@
-import com.ooyala.flex.jobexecutor.dto.FlexObjectReference
-import com.ooyala.flex.plugins.GroovyScriptContext
-import com.ooyala.flex.sdk.FlexSdkClient
-
-
-def execute(FlexSdkClient flexSdkClient, GroovyScriptContext context, ScriptingLockManager lockManager,
-            List<FlexObjectReference> objectRefs) {
+def execute() {
     return "true"
 }
