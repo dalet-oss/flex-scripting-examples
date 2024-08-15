7f38b578b63f02deea3f5899bb836b31e22ba575
Mackenzie Sepler <msepler@dalet.com>
2024-08-15 13:06:37 +0100

    update import

--- UsingAnIdeToWriteScripts/src/main/groovy/PollForResultsScript.groovy
+++ UsingAnIdeToWriteScripts/src/main/groovy/PollForResultsScript.groovy
@@ -1,7 +1,7 @@
 import com.ooyala.flex.jobexecutor.dto.FlexObjectReference
 import com.ooyala.flex.plugins.GroovyScriptContext
 import com.ooyala.flex.sdk.FlexSdkClient
-import com.ooyala.flex.plugins.actions.lock.ScriptingLockManager
+import com.ooyala.flex.plugins.actions.service.scripting.lock.ScriptingLockManager
 
 
 def execute(FlexSdkClient flexSdkClient, GroovyScriptContext context, ScriptingLockManager lockManager,
