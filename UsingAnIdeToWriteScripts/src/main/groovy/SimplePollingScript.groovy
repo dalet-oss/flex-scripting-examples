ff5f5c97cef7286a46ae110eac438ab127135c27
Mackenzie Sepler <msepler@dalet.com>
2024-09-23 10:17:40 +0100

    include httpresponse import

--- UsingAnIdeToWriteScripts/src/main/groovy/SimplePollingScript.groovy
+++ UsingAnIdeToWriteScripts/src/main/groovy/SimplePollingScript.groovy
@@ -1,12 +1,11 @@
 import com.ooyala.flex.jobexecutor.dto.FlexObjectReference
-import com.ooyala.flex.model.asset.create.NewAssetPlaceholder
 import com.ooyala.flex.plugins.GroovyScriptContext
 import com.ooyala.flex.plugins.actions.service.scripting.lock.ScriptingLockManager
+import com.ooyala.flex.plugins.actions.impl.message.http.response.HttpResponse
 import com.ooyala.flex.sdk.FlexSdkClient
 
 
 def execute(FlexSdkClient flexSdkClient, GroovyScriptContext context, ScriptingLockManager lockManager,
-            List<FlexObjectReference> objectRefs) {
-
-    return "true"
+            List<FlexObjectReference> objectRefs, HttpResponse httpResponse) {
+    return true
 }
